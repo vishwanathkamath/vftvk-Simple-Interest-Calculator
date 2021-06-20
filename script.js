9a5804dceff906bae049fdc0cb34073fc57c5fa0
Vishwanath Pethri Kamath <vishwanathkamath@gmail.com>
2021-06-20 09:47:43 +0000

    Submit

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 function calculate () {
     var p1 = document.getElementById("savingAmount").value;
-    var p2 = document.getElementById("numberofYears").value;
-    var p3 = document.getElementById("interestValue").value;
+    var p2 = document.getElementById("interestValue").value;
+    var p3 = document.getElementById("numberofYears").value;
     if(p1<=0||p2<=0||p3<=0){
         reset();
         document.getElementById("output").innerHTML = "Enter positive values!!";
     } else{
-        document.getElementById("output1").innerHTML = "If you deposit " + p1 +",";
-        document.getElementById("output2").innerHTML = "at an interest rate of " + p3 +"%.";
+        document.getElementById("output1").innerHTML = "If you deposit ";
+        document.getElementById("amt").innerHTML = p1 +",";
+        document.getElementById("output2").innerHTML = "at an interest rate of ";
+        document.getElementById("int").innerHTML = p2 +"%.";
         ret = p1*p2*(p3/100);
-        document.getElementById("output3").innerHTML = "You will recieve an amount of " + ret + ",";
+        document.getElementById("output3").innerHTML = "You will recieve an amount of ";
+        document.getElementById("ret").innerHTML = ret + ",";
         da = new Date().getFullYear();
-        yr = parseInt(p2)+da;
-        document.getElementById("output").innerHTML = "in the year " + yr;
+        yr = parseInt(p3)+da;
+        document.getElementById("output").innerHTML = "in the year ";
+        document.getElementById("yrs").innerHTML = yr;
     }
     
     
